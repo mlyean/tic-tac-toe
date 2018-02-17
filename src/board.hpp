500a3033912338aef0f59c0f059f86b862c68b30
Lim Ming Yean <mingyean@hotmail.com>
2018-02-17 21:33:12 +0800

    Added board header

---
+++ src/board.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <array>
+#include <string>
+
+#include "move.hpp"
+#include "piece.hpp"
+
+class Board
+{
+    std::array<Piece, 9> board;
+
+    void makeMove(Move);
+    std::string str() const;
+}
