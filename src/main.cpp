3eb3a7e7762c2881244799d9ef0d7f88c837ab2a
Lim Ming Yean <mingyean@hotmail.com>
2018-02-18 18:13:14 +0800

    Request player name

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,15 @@
 int main() {
     std::cout << "Welcome to Tic-Tac-Toe!\n";
 
+    std::string player1Name, player2Name;
+    std::cout << "What's your name Player 1? ";
+    std::cin >> player1Name;
+    std::cout << "What's your name Player 2? ";
+    std::cin >> player2Name;
+
     while (true)
     {
-        Game game(new HumanPlayer(1, Piece::KNOT), new HumanPlayer(2, Piece::CROSS));
+        Game game(new HumanPlayer(1, Piece::KNOT, player1Name), new HumanPlayer(2, Piece::CROSS, player2Name));
 
         while (game.getState() == Game::GameState::IN_PROGRESS)
         {
