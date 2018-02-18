2bdc60cdcaa4854b02a650215a7777594f3d0cd9
Lim Ming Yean <mingyean@hotmail.com>
2018-02-18 14:52:07 +0800

    Add endless loop

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,23 @@
 int main() {
     std::cout << "Welcome to Tic-Tac-Toe!\n\n";
 
-    Game game(new HumanPlayer(1, Piece::KNOT), new HumanPlayer(2, Piece::CROSS));
-
-    while (game.getState() == Game::GameState::IN_PROGRESS)
+    while (true)
     {
-        try {
-            game.next();
+        Game game(new HumanPlayer(1, Piece::KNOT), new HumanPlayer(2, Piece::CROSS));
+
+        while (game.getState() == Game::GameState::IN_PROGRESS)
+        {
+            try {
+                game.next();
+            }
+            catch (std::exception &e)
+            {
+                std::cout << "Invalid move detected, try again\n";
+            }
         }
-        catch (std::exception &e) {}
+
+        std::cout << "\nGame over!\n";
     }
-    
+
     return 0;
 }
