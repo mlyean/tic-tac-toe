184110fdab980ef65dcae9eccff6d93a39efab6b
Lim Ming Yean <mingyean@hotmail.com>
2018-02-18 13:07:58 +0800

    Implement HumanPlayer

---
+++ src/player_human.cpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+
+#include "player_human.hpp"
+
+HumanPlayer::HumanPlayer(int id, Piece piece) : Player(id, piece) {}
+
+Move HumanPlayer::getMove(const Board& board)
+{
+    int i, j;
+    std::cout << "Board state:\n" << board.str() << "\nYour move?";
+    std::cin >> i >> j;
+    return {i, j, piece};
+}
