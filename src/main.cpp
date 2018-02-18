474f4ab8eaeaad9c7762e3734440030b8ae4f14f
Lim Ming Yean <mingyean@hotmail.com>
2018-02-18 14:03:21 +0800

    Add basic client

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+#include "game.hpp"
+#include "player_human.hpp"
+
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "Welcome to Tic-Tac-Toe!\n";
+
+    Game game(new HumanPlayer(1, Piece::KNOT), new HumanPlayer(2, Piece::CROSS));
+
+    while (game.getState() == Game::GameState::IN_PROGRESS)
+    {
+        game.next();
+    }
     
     return 0;
 }
