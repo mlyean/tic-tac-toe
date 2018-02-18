05c954dc95bd3b3e6bd3f29c99680c398ac3c248
Lim Ming Yean <mingyean@hotmail.com>
2018-02-18 11:03:51 +0800

    Modify str

--- src/player.cpp
+++ src/player.cpp
@@ -7,6 +7,6 @@ Player::Player(int id, Piece piece) : id(id), piece(piece) {}
 std::string Player::str() const
 {
     std::ostringstream oss;
-    oss << "id: " << id << ", piece: " << static_cast<char>(piece);
+    oss << "{ id: " << id << ", piece: " << static_cast<char>(piece) << " }";
     return oss.str();
 }
