191ebff65c14d4e4b3ff7d3b8afe7ab105b1a1fe
Lim Ming Yean <mingyean@hotmail.com>
2018-02-19 08:33:33 +0800

    Adjust spacing

--- src/player_random.cpp
+++ src/player_random.cpp
@@ -1,8 +1,10 @@
 #include "player_random.hpp"
 
-RandomPlayer::RandomPlayer(int id, Piece piece) : Player(id, piece), gen(rd()), dis(0, 2) {}
+RandomPlayer::RandomPlayer(int id, Piece piece) :
+    Player(id, piece), gen(rd()), dis(0, 2) {}
 
-RandomPlayer::RandomPlayer(int id, Piece piece, const std::string& name) : Player(id, piece, name), gen(rd()), dis(0, 2) {}
+RandomPlayer::RandomPlayer(int id, Piece piece, const std::string& name) :
+    Player(id, piece, name), gen(rd()), dis(0, 2) {}
 
 RandomPlayer::~RandomPlayer() {}
 
