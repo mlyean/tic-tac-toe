b986ed892963be3d5cc1eadf93060f5b0a0bb440
Lim Ming Yean <mingyean@hotmail.com>
2018-02-19 08:43:54 +0800

    Change formatting

--- src/client.cpp
+++ src/client.cpp
@@ -40,13 +40,13 @@ void TicTacToeClient::run()
 {
     while (true)
     {
-        Game game(new HumanPlayer(1, Piece::KNOT, player1Name), new RandomPlayer(2, Piece::CROSS, player2Name));
+        Game game(new HumanPlayer(1, Piece::KNOT, player1Name), new HumanPlayer(2, Piece::CROSS, player2Name));
 
         while (game.getState() == Game::GameState::IN_PROGRESS)
         {
             std::cout << game.board.str() << "\n\n";
             try { game.next(); }
-            catch (std::exception &e) { std::cout << "\nInvalid move detected, try again\n"; }
+            catch (std::exception &e) { std::cout << "Invalid move detected, try again\n\n"; }
         }
 
         switch (game.getState()) {
